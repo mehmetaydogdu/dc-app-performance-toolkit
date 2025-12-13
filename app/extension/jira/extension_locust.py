aef2fcd7a1ad1cf5720ca4272c9c6903846d0c55
Mehmet Aydogdu <mehmet@bloompeak.io>
2025-12-13 18:53:50 +0300

    status-time-free custom actions

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -5,28 +5,28 @@ logger = init_logger(app_type='jira')
 
 
 @jira_measure("locust_app_specific_action")
-# WebSudo is a feature that enhances security by requiring administrators to re-authenticate before
-# accessing administrative functions within Atlassian applications.
-# do_websudo=True requires user administrative rights, otherwise requests fail.
-#@run_as_specific_user(username='admin', password='admin', do_websudo=False)  # run as specific user
+# @run_as_specific_user(username='admin', password='admin')  # run as specific user
 def app_specific_action(locust):
-    r = locust.get('/app/get_endpoint', catch_response=True)  # call app-specific GET endpoint
+    r = locust.get('/plugins/servlet/bloompeak-stf/mainservlet/st-report', catch_response=True)  # call app-specific GET endpoint
     content = r.content.decode('utf-8')   # decode response content
+    assert '<div id="bloompeak-root" class="ac-content"></div>' in content
 
-    token_pattern_example = '"token":"(.+?)"'
-    id_pattern_example = '"id":"(.+?)"'
-    token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
-    id = re.findall(id_pattern_example, content)    # get ID from response using regexp
+    r = locust.get('/download/resources/io.bloompeak.status-time-free:ui-i18n/en.json', catch_response=True)  # call app-specific GET endpoint
+    content = r.content.decode('utf-8')   # decode response content
+    assert '"Search": "Search",' in content
 
-    logger.locust_info(f'token: {token}, id: {id}')  # log info for debug when verbose is true in jira.yml file
-    if 'assertion string' not in content:
-        logger.error(f"'assertion string' was not found in {content}")
-    assert 'assertion string' in content  # assert specific string in response content
+    locust.get('/download/resources/io.bloompeak.status-time-free:ui-i18n/en-US.json', catch_response=False)
+    locust.get('/rest/api/2/myself', catch_response=False)
+    locust.get('/rest/api/2/field', catch_response=False)
+    locust.get('/rest/api/2/filter/favourite', catch_response=False)
+    locust.get('/rest/io.bloompeak.stf/1.0/status', catch_response=False)
+    locust.get('/rest/io.bloompeak.stf/1.0/calendars', catch_response=False)
+    locust.get('/rest/io.bloompeak.stf/1.0/reports', catch_response=False)
+    
+    locust.get('/download/resources/io.bloompeak.status-time-free:react-css/2.81e8548a.chunk.css', catch_response=False)
+    locust.get('/download/resources/io.bloompeak.status-time-free:react-css/main.afa36abd.chunk.css', catch_response=False)
 
-    body = {"id": id, "token": token}  # include parsed variables to POST request body
-    headers = {'content-type': 'application/json'}
-    r = locust.post('/app/post_endpoint', body, headers, catch_response=True)  # call app-specific POST endpoint
-    content = r.content.decode('utf-8')
-    if 'assertion string after successful POST request' not in content:
-        logger.error(f"'assertion string after successful POST request' was not found in {content}")
-    assert 'assertion string after successful POST request' in content  # assertion after POST request
+    locust.get('/download/resources/io.bloompeak.status-time-free:react-js/2.7bc545d9.chunk.js', catch_response=False)
+    locust.get('/download/resources/io.bloompeak.status-time-free:react-js/3.ff963ffd.chunk.js', catch_response=False)
+    locust.get('/download/resources/io.bloompeak.status-time-free:react-js/main.5f65c7c0.chunk.js', catch_response=False)
+    locust.get('/download/resources/io.bloompeak.status-time-free:react-js/runtime-main.b057ef5a.js', catch_response=False)
--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -13,37 +13,25 @@ def app_specific_action(webdriver, datasets):
     if datasets['custom_issues']:
         issue_key = datasets['custom_issue_key']
 
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out action
-    #
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.wait_for_login_page_loaded()
-    #         login_page.set_credentials(username=username, password=password)
-    #         login_page.wait_for_dashboard_or_first_login_loaded()
-    #         if login_page.is_first_login():
-    #             login_page.first_login_setup()
-    #         if login_page.is_first_login_second_page():
-    #             login_page.first_login_second_page_setup()
-    #         login_page.wait_for_page_loaded()
-    #         # uncomment below line to do web_sudo and authorise access to admin pages
-    #         # AdminPage(webdriver).go_to(password=password)
-    #
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
-
-    @print_timing("selenium_app_custom_action")
+    @print_timing("selenium_app_custom_action_view_issue")
     def measure():
         @print_timing("selenium_app_custom_action:view_issue")
         def sub_measure():
             page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
             page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.ID, "bloompeak-stf-issue-detail-frame"))  # Wait for you app-specific UI element by ID selector
+        sub_measure()
+    measure()
+
+def app_specific_action_report_page(webdriver, datasets):
+    page = BasePage(webdriver)
+    @print_timing("selenium_app_custom_action_report_page")
+    def measure():
+
+        @print_timing("selenium_app_custom_action:report_page")
+        def sub_measure():
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/plugins/servlet/bloompeak-stf/mainservlet/st-dashboard?dashboardItemId=10101&dashboardId=10100")
+            page.wait_until_visible((By.CSS_SELECTOR, '#bloompeak-table-result-10102'))  # Wait for you app-specific UI element by ID selector
         sub_measure()
     measure()
 
